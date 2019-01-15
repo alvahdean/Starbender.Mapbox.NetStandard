32208946118d075309e709b953b74df2d9313933
alvahdean <alvahdean@gmail.com>
2019-01-14 22:00:41 -0600

    Initial commit of NetStandard library & Tests

---
+++ Mapbox.Test.NUnit/AppConfig.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mapbox.Test.NUnit
+{
+    using Microsoft.Extensions.Configuration;
+
+    public static class AppConfig
+    {
+        private const string MapboxApiKeyEnvironmentVar = "MAPBOX_ACCESS_TOKEN";
+
+        private static IConfigurationRoot _config;
+
+        private static string _mapboxApiKey;
+
+        static AppConfig()
+        {
+            _mapboxApiKey = Environment.GetEnvironmentVariable(MapboxApiKeyEnvironmentVar);
+            if (!string.IsNullOrWhiteSpace(_mapboxApiKey))
+            {
+                Console.WriteLine($"Using {MapboxApiKeyEnvironmentVar}={_mapboxApiKey}");
+            }
+
+            _config = new ConfigurationBuilder().AddJsonFile("appSettings.json", false).Build();
+            _mapboxApiKey = _config["Mapbox:ApiKey"];
+            if (string.IsNullOrWhiteSpace(_mapboxApiKey))
+            {
+                throw new KeyNotFoundException("Mapbox API Key not found in appSettings.json");
+            }
+
+            Console.WriteLine($"Using {MapboxApiKeyEnvironmentVar}={_mapboxApiKey}");
+            Environment.SetEnvironmentVariable(MapboxApiKeyEnvironmentVar, _mapboxApiKey);
+        }
+
+        public static string MapboxApiKey => _mapboxApiKey;
+
+        public static string StringValue(string var) => _config[var];
+
+        public static bool BoolValue(string var) => bool.Parse(_config[var]);
+
+        public static int IntValue(string var) => int.Parse(_config[var]);
+
+        public static double DoubleValue(string var) => double.Parse(_config[var]);
+    }
+}
---
+++ Mapbox.Test.NUnit/CanonicalTileIdTest.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="CanonicalTileIdTest.cs" company="Mapbox">
+//     Copyright (c) 2016 Mapbox. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Mapbox.UnitTest
+{
+    using Mapbox.Map;
+    using Mapbox.Utils;
+    using NUnit.Framework;
+
+    [TestFixture]
+	internal class CanonicalTileIdTest
+	{
+		[Test]
+		public void ToVector2d()
+		{
+			var set = TileCover.Get(Vector2dBounds.World(), 5);
+
+			foreach (var tile in set)
+			{
+				var reverse = TileCover.CoordinateToTileId(tile.ToVector2d(), 5);
+
+				Assert.AreEqual(tile.Z, reverse.Z);
+				Assert.AreEqual(tile.X, reverse.X);
+				Assert.AreEqual(tile.Y, reverse.Y);
+			}
+		}
+	}
+}
